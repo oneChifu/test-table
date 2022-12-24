93684511f594394dbb2834e5495fd63f097a3cc8
Pavlo Lavrus <onechifu@gmail.com>
2022-12-24 19:10:06 +0200

    Add dist directory for deploy

---
+++ dist/_nuxt/static/1671901400/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Ftest-table\u002F_nuxt\u002Fstatic\u002F1671901400",layout:"default",error:a,state:{todos:{filter:{title:b,userId:b}}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-table\u002F",assetsPath:"\u002Ftest-table\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
