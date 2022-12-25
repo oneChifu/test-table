276b3f7906ce2339eb04313d8530d409260560a6
Pavlo Lavrus <onechifu@gmail.com>
2022-12-25 21:45:30 +0200

    Fix for deploy gh-pages

--- dist/_nuxt/b556499.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{317:function(n,t,e){"use strict";e.r(t);var o={name:"IndexPage"},l=e(46),component=Object(l.a)(o,(function(){var n=this._self._c;return n("div",{staticClass:"container"},[n("Todos")],1)}),[],!1,null,null,null);t.default=component.exports;installComponents(component,{Todos:e(316).default})}}]);
