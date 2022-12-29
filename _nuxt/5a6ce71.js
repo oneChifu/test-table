89b62a6b431b967386636f14858c3381ebe31c03
Pavlo Lavrus <onechifu@gmail.com>
2022-12-29 06:13:14 +0200

    9b8c87d

---
+++ _nuxt/5a6ce71.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{350:function(n,t,e){"use strict";e.r(t);var o={name:"IndexPage"},l=e(46),component=Object(l.a)(o,(function(){var n=this._self._c;return n("div",{staticClass:"container"},[n("Todos")],1)}),[],!1,null,null,null);t.default=component.exports;installComponents(component,{Todos:e(348).default})}}]);
